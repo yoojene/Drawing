5c14cc5d2744a7de40fb4dc08012d6d5b3546fd4
Eugene Cross <eugene.cross@gmail.com>
2023-08-27 18:34:37 +0100

    HWS Day 46: Drawing.  Challenge 1 & 2

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+struct Arrow: Shape {
+
+    
+    // I struggled with the rect.minX, rect.maxY relative to the shapes rect
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 200, y: 100))
+        path.addLine(to: CGPoint(x: 100, y: 300))
+        path.addLine(to: CGPoint(x: 300, y: 300))
+        path.addLine(to: CGPoint(x: 200, y: 100))
+//        path.fill(.blue)
+        path.closeSubpath()
+        path.move(to: CGPoint(x: 200, y: 300))
+        path.addLine(to: CGPoint(x: 200, y: 603) )
+
+
+        return path
+
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var thickness: CGFloat = 10.0
     var body: some View {
+    
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Arrow()
+                .stroke(.blue, style: StrokeStyle(lineWidth: thickness))
+                .onTapGesture {
+                    withAnimation(.easeIn(duration: 1)) {
+                        thickness =  Double.random(in: 10...100.0)
+                    }
+                   
+                }
+            
+            Slider(value: $thickness, in: 1...100.0, onEditingChanged: { value in
+                withAnimation {
+                    print("slider edit \(thickness)")
+                }
+            })
+            .padding()
+            
         }
-        .padding()
+            
+      
     }
 }
 
